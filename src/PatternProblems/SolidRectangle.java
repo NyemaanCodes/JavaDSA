d3ac944bac112ed7a061b2c256578649ac39e8a2
NyemaanCodes <nyemaansk@gmail.com>
2025-01-25 19:29:19 +0530

    Added 2 problem Folder

--- src/PatternProblems/SolidRectangle.java
+++ src/PatternProblems/SolidRectangle.java
@@ -1,4 +1,35 @@
 package PatternProblems;
 
+import java.util.Scanner;
+
 public class SolidRectangle {
+    /*
+    Print a Solid Rectangle, given the only breadth of rectangle
+    let n=5
+    o/p:
+
+            *****
+            *****
+            *****
+            *****
+     */
+
+    public static void main(String[] args){
+
+        //Take input from user
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.println("Enter n: ");
+        n = sc.nextInt();
+
+        for(int i= 0 ; i< n-1 ; i++){
+            for (int j = 0 ; j<n ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
