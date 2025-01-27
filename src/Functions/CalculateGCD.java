700b298f802268ff5bd6f9cb66b0ff62f970a801
NyemaanCodes <nyemaansk@gmail.com>
2025-01-27 17:29:37 +0530

    Added Function programs

---
+++ src/Functions/CalculateGCD.java
@@ -0,0 +1,32 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class CalculateGCD {
+
+    // Function to calculate GCD of two numbers
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Prompting the user to enter two numbers
+        System.out.println("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        // Calculating GCD and displaying the result
+        int result = gcd(num1, num2);
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + result);
+    }
+
+}
